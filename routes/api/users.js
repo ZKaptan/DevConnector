12cf0587d675a7cd72e5f64c1a61d957ceeb6e31
ZKaptan <dkkaptan20@gmail.com>
2021-06-13 19:55:06 -0300

    implemented JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -55,11 +57,28 @@ router.post(
 
 			user.password = await bcrypt.hash(password, salt);
 
+			// Save user in database
 			await user.save();
 
 			// return jsonwebtoken
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
 
-			res.send("User registered");
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) {
+						throw err;
+					} else {
+						res.json({ token });
+					}
+				}
+			);
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send("Server error");

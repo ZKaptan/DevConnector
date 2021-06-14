a33a772d7bd4b7aa0609e06446db8a94a28ef822
ZKaptan <dkkaptan20@gmail.com>
2021-06-14 10:03:03 -0300

    Created profile schema

--- config/db.js
+++ config/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
 			useUnifiedTopology: true,
 			useCreateIndex: true,
 		});
-		console.log("MongoDB connected");
+		console.log("MongoDB connected...");
 	} catch (err) {
 		console.error(err.message);
 		// Exit process with failure

269503accc3134bc161ebf51fe83af2482d1a730
ZKaptan <dkkaptan20@gmail.com>
2021-06-23 10:38:45 -0300

    Added ability to edit profile and create profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import DashBoardActions from "./DashboardActions";
 import { getCurrentProfile } from "../../actions/profile";
+import DashboardActions from "./DashboardActions";
 
 const Dashboard = ({
 	getCurrentProfile,
@@ -24,7 +26,9 @@ const Dashboard = ({
 				<i className="fas fa-user"></i> Welcome {user && user.name}
 			</p>
 			{profile !== null ? (
-				<Fragment>has </Fragment>
+				<Fragment>
+					<DashboardActions />
+				</Fragment>
 			) : (
 				<Fragment>
 					<p>You have not yet set up a profile, please add some info</p>

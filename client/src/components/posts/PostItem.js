251bf0544b6f9220a33330e15ee422d413d45233
ZKaptan <dkkaptan20@gmail.com>
2021-06-29 10:01:55 -0300

    User can now remove their own post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 import Moment from "react-moment";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { addLike, removeLike } from "../../actions/post";
+import { addLike, removeLike, deletePost } from "../../actions/post";
 
 const PostItem = ({
 	auth,
 	addLike,
 	removeLike,
+	deletePost,
 	post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
 	return (
@@ -47,7 +48,11 @@ const PostItem = ({
 					)}{" "}
 				</Link>
 				{!auth.loading && user === auth.user._id && (
-					<button type="button" class="btn btn-danger">
+					<button
+						onClick={() => deletePost(_id)}
+						type="button"
+						class="btn btn-danger"
+					>
 						<i class="fas fa-times"></i>
 					</button>
 				)}
@@ -60,6 +65,7 @@ PostItem.propTypes = {
 	post: PropTypes.object.isRequired,
 	auth: PropTypes.object.isRequired,
 	addLike: PropTypes.func.isRequired,
+	deletePost: PropTypes.func.isRequired,
 	removeLike: PropTypes.func.isRequired,
 };
 
@@ -67,4 +73,6 @@ const mapStateToProps = (state) => ({
 	auth: state.auth,
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+	PostItem
+);

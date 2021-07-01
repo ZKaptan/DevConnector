77331eb8286105dc3285c4407d483ed5588517aa
ZKaptan <dkkaptan20@gmail.com>
2021-07-01 20:21:05 -0300

    Changed input field values

--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -90,8 +90,8 @@ const EditProfile = ({
 						<option value="Junior Developer">Junior Developer</option>
 						<option value="Senior Developer">Senior Developer</option>
 						<option value="Manager">Manager</option>
-						<option value="Student or Learning">Student or Learning</option>
-						<option value="Instructor">Instructor or Teacher</option>
+						<option value="Student or Learning">Student</option>
+						<option value="Instructor">Teacher</option>
 						<option value="Intern">Intern</option>
 						<option value="Other">Other</option>
 					</select>
